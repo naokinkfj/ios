8854cf2641b7f3ef1e6304658f3cec74d10ca59a
Masahiro Hori <good.bye.baseball.17@gmail.com>
2016-01-24 19:27:07 +0900

    アラート機能を追加

--- AlertSample/AlertSample/ViewController.swift
+++ AlertSample/AlertSample/ViewController.swift
@@ -30,6 +30,7 @@ class ViewController: UIViewController {
         
         self.presentViewController(alertController,animated: true, completion: nil)
     }
+    
 
     @IBAction func alertButton2(sender: UIButton) {
         
@@ -48,10 +49,7 @@ class ViewController: UIViewController {
         
         let alertController = UIAlertController(title: "Hello!", message: "This is Alert Sample", preferredStyle: .Alert)
         
-//        let defaultAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
-//        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
-        
-        let defaultAction = UIAlertAction(title: "OK", style: .Default){
+        let okAction = UIAlertAction(title: "OK", style: .Default){
             action in print("Pushed OK!!")
         }
         
@@ -60,10 +58,64 @@ class ViewController: UIViewController {
         }
         
         
-        alertController.addAction(defaultAction)
+        alertController.addAction(okAction)
         alertController.addAction(cancelAction)
         
         self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func alertButton4(sender: UIButton) {
+        let alertController = UIAlertController(title: "Hello!", message: "This is Alert Sample", preferredStyle: .ActionSheet)
         
+        let firstAction = UIAlertAction(title: "First", style: .Default){
+            action in print("Pushed First!!")
+        }
+        
+        let secondAction = UIAlertAction(title: "Second", style: .Default){
+            action in print("Pushed Second!!")
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel){
+            action in print("Pushed Cancel!!")
+        }
+        
+        alertController.addAction(firstAction)
+        alertController.addAction(secondAction)
+        alertController.addAction(cancelAction)
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func alertButton5(sender: UIButton) {
+        
+//        var inputTextField: UITextField?
+//        var passwordField: UITextField?
+        
+        let alertController = UIAlertController(title: "Login", message: "Input your ID and Password", preferredStyle: .Alert)
+        
+        let cancelAction: UIAlertAction = UIAlertAction(title: "Cancel", style: .Cancel){
+            action -> Void in print("Pushed CANCEL")
+        }
+        alertController.addAction(cancelAction)
+        
+        let loginAction: UIAlertAction = UIAlertAction(title: "Loing", style: .Default){
+            action -> Void in print("Pushed Login")
+        }
+        alertController.addAction(loginAction)
+        
+        alertController.addTextFieldWithConfigurationHandler { textField -> Void in
+//            inputTextField = textField
+            textField.placeholder = "ID"
+        }
+        
+        alertController.addTextFieldWithConfigurationHandler { textField -> Void in
+//            passwordField = textField
+            textField.secureTextEntry = true
+            textField.placeholder = "password"
+        }
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
     }
 }
